599649b0b629eae7723f06919cf7480a85df542b
Cameron Kluza <cmkluza@gmail.com>
2020-08-04 17:04:58 -0400

    Adds Hall sensor conversion methods and fixes Hall sensor sim

--- firmware/src/hall_sensor/hall_sensor.hpp
+++ firmware/src/hall_sensor/hall_sensor.hpp
@@ -29,4 +29,25 @@ struct HallSensor {
      * @return the most recently read sample.
      */
     type read();
+
+    /**
+     * Converts a sensor type to a 2-byte buffer.
+     *
+     * @param[in] value the sensor value to convert.
+     * @param[in] buffer the buffer to write into.
+     */
+    static inline void to_bytes(type value, std::uint8_t *buffer) {
+        buffer[0] = (value >> 0) & 0xFF;
+        buffer[1] = (value >> 8) & 0xFF;
+    }
+
+    /**
+     * Converts a byte buffer into a sensor reading value.
+     *
+     * @param[in] buffer the byte buffer to convert from.
+     * @return    the converted value.
+     */
+    static inline type from_bytes(const std::uint8_t *buffer) {
+        return static_cast<type>(buffer[0] | (buffer[1] << 8));
+    }
 };
--- firmware/src/hall_sensor/hall_sensor_sim.cpp
+++ firmware/src/hall_sensor/hall_sensor_sim.cpp
@@ -27,11 +27,6 @@ static constexpr std::uint32_t SENSOR_MIN_VAL =
 /** Maximum value the sensor could return. */
 static constexpr std::uint32_t SENSOR_MAX_VAL =
     static_cast<std::uint32_t>((SENSOR_MAX_V / SENSOR_REF_V) * (1 << MEASUREMENT_BITS));
-/** Expected precision of the sensor readings. */
-static constexpr std::uint32_t SENSOR_PRECISION = {
-    static_cast<std::uint32_t>(
-        (1 << MEASUREMENT_BITS) * (SENSOR_REF_V - (SENSOR_MAX_V - SENSOR_MIN_V)))
-};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Private Data
@@ -47,7 +42,7 @@ static sensorsim_state_t sim_state;
 void HallSensor::init() {
     sim_cfg.min          = SENSOR_MIN_VAL;
     sim_cfg.max          = SENSOR_MAX_VAL;
-    sim_cfg.incr         = SENSOR_PRECISION;
+    sim_cfg.incr         = 0x10;
     sim_cfg.start_at_max = true;
 
     sensorsim_init(&sim_state, &sim_cfg);
